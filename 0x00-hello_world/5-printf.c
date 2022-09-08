ee216ecd985664bddbf3c34dc278e15847b16dc5
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-08 11:44:47 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ * main: this the entry point
+ *
+ * description: it prints : "with proper grammar, but this outcome is a piece of art,\n
+ *
+ * return
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but this outcome is a piece of art,\n");
+		return (0);
+}
