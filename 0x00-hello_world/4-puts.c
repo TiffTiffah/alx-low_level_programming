dd8513e645d578664d67addb568ce6543f9e804a
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-08 11:31:10 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void) /*this the entry point */
+	
+{
+	
+puts("\"Programming is like building a multilingual puzzle"); /*This is the simple statement*/
+return (0); /*end of the function */
+			
+}
