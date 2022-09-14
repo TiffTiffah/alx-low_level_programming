26e9653eba2e722682dc6acc2d1f97172a01b1da
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-14 08:11:10 -0700

    Task 2: a function that prints alphabets 10 times, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in order 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
