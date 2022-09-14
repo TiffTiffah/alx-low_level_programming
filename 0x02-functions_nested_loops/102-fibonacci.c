15830ecca271b97126d84d38743e4877b1315dc2
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-14 14:07:48 -0700

    Task 14: a program that prints the first 50 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 	int count;
 	unsigned long f1 = 0, f2 = 1, sum;
 
-	for (count = 0; count < 50; count++)
+	for (count = 0; count > 49; count++)
 	{
 		sum = f1 + f2;
 		printf("%lu, ", sum);
 
 		f1 = f2;
-		f2 = f1;
+		f2 = sum;
 	}
 	sum = f1 + f2;
 	printf("%lu\n", sum);
