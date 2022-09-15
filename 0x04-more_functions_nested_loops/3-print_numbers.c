3b21cfbe4b82af5da952e59630af656627b694f1
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-15 09:03:49 -0700

    Task 3: prints integers from 0 to 9 followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 9)
+	{
+		_putchar(num);
+
+		num++;
+	}
+
+	_putchar('\n');
+}
