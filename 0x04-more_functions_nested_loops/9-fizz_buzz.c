8425e2e7873f62a3268b2c754dcaef7eb45ae4be
Latifa Abdallarehan <99710566+TiffTiffah@users.noreply.github.com>
2022-09-15 21:35:44 +0300

    Task 9:  a program that prints the numbers from 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,22 @@
  */
 int main(void)
 {
-	int num = 0;
+	int num;
 
-	while (num <= 100)
+	for (num = 1; num < 101; num++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
-
-		else if ((num % 3) == 0)
-			printf("Fizz");
-
-		else if ((num % 5) == 0)
-			printf("Buzz");
-
+		if (num % 5 == 0 && num % 3 == 0)
+			printf("FizzBuzz ");
+		else if (num % 5 == 0)
+			if (num == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		else if (num % 3 == 0)
+			printf("Fizz ");
 		else
-			printf("%d", num);
-
-		if (num == 100)
-			continue;
-		printf(" ");
-    
-    num++;
+			printf("%d ", num);
 	}
-
 	printf("\n");
 
 	return (0);
