89c886c6baeb45acd65cbaa5f7dc4ac7fc9b21f0
Latifa Abdallarehan <99710566+TiffTiffah@users.noreply.github.com>
2022-09-19 21:47:37 +0300

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,6 +6,15 @@
  * @s: the string to be reversed
  *
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
+int len = 0, i;
 
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
