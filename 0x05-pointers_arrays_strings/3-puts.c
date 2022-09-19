e0d82083331c622a73ee967b31a3a2ea3e2df5bc
Latifa Abdallarehan <tiffahnick012@gmail.com>
2022-09-19 11:40:53 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
