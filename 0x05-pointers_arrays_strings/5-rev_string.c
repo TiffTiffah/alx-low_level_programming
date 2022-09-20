76429cbe8f25c59e4fbf52c41b4697b5fcb441fe
Latifa Abdallarehan <99710566+TiffTiffah@users.noreply.github.com>
2022-09-20 19:48:17 +0300

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+include <stdio.h>
+#include "main.h"
+/**
+* rev_string - function to reverse words
+* @s : - the parameter to reverse words
+*/
+void rev_string(char *s)
+{
+
+	int a = 0, b = 0;
+	char str[500];
+
+	while (*(s + a))
+	{
+		*(str + a) = *(s + a);
+		i++;
+	}
+	
+	a = a - 1;
+	while (a >= 0)
+	{
+		*(s + a) = *(str + b);
+		b++;
+		a--;
+	}
+}
