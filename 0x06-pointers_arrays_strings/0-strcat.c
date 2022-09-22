84083a142cd6c186c9edd6fcea6f9e3fef303f46
Latifa Abdallarehan <99710566+TiffTiffah@users.noreply.github.com>
2022-09-22 20:40:55 +0300

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strcat - Concatenates two strings
+ * @dest: pointer to the string to be concatenated upon.
+ * @src: source string to be appended to @dest.
+ *
+ * Return: pointer to the destination string @dest.
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0;
+  dest_len = 0;
+
+	while (*(dest + index))
+		index++;
+
+	while ((*(dest + index) = *(src + dest_len)))
+  {
+    i++;
+    j++;
+  }
+
+	return (dest);
+}
